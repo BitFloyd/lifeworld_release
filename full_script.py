dcbaba26b011f0080ca95a102615c1726f15199b
Seby <sby.jcub@gmail.com>
2017-05-09 10:17:08 -0400

    inpainting_root setup

--- full_script.py
+++ full_script.py
@@ -2,17 +2,18 @@ from data_package import data_fns
 from exec_functions import execute
 
 # Make this folder if it doesnt exist. Path to save the pkled datasets and captions
-save_path = '/usr/local/data/sejacob/lifeworld/data/inpainting/pkls/'
+inpainting_root = '/usr/local/data/sejacob/lifeworld/data/inpainting/'
+save_path = inpainting_root + 'pkls/'
 
 # This is where the training images are
-path = '/usr/local/data/sejacob/lifeworld/data/inpainting/train2014/'
+path = inpainting_root + 'train2014/'
 train_or_val = 'train'
 
 data_fns.save_dataset(path, save_path, train_or_val)
 data_fns.make_input_output_set(save_path, train_or_val)
 
 # This is where the validation images are
-path = '/usr/local/data/sejacob/lifeworld/data/inpainting/val2014/'
+path = inpainting_root + 'val2014/'
 train_or_val = 'val'
 
 data_fns.save_dataset(path, save_path, train_or_val)
@@ -20,30 +21,30 @@ data_fns.make_input_output_set(save_path, train_or_val)
 
 # Path to the trained google gensim model
 # http://mccormickml.com/2016/04/12/googles-pretrained-word2vec-model-in-python/
-gensimpath = '/usr/local/data/sejacob/lifeworld/data/inpainting/word2vec/GoogleNews-vectors-negative300.bin'
+gensimpath = inpainting_root+'word2vec/GoogleNews-vectors-negative300.bin'
 
-trainpath = '/usr/local/data/sejacob/lifeworld/data/inpainting/train2014/'
-valpath = '/usr/local/data/sejacob/lifeworld/data/inpainting/val2014/'
+trainpath = inpainting_root+'train2014/'
+valpath = inpainting_root+'val2014/'
 
 # This is where the unprocessed captions are
-captionpath = '/usr/local/data/sejacob/lifeworld/data/inpainting/dict_key_imgID_value_caps_train_and_valid.pkl'
+captionpath = inpainting_root+'dict_key_imgID_value_caps_train_and_valid.pkl'
 pklpath = save_path
 
 data_fns.captionsetup(gensimpath, trainpath, valpath, captionpath, pklpath)
 
 # This is where the training losses will be plotted
-plt_path = '/usr/local/data/sejacob/lifeworld/data/inpainting/predictions/dcg_losses.png'
+plt_path = inpainting_root+'predictions/dcg_losses.png'
 
 # Predictions made on one batch of the training set after each 100th iteration
-wr_path = '/usr/local/data/sejacob/lifeworld/data/inpainting/predictions/train_test_run_train_batch'
+wr_path = inpainting_root+'predictions/train_test_run_train_batch'
 
 # Path to the directory where predictions will be made on the whole test and train set after each run
-wr_predictions_path = '/usr/local/data/sejacob/lifeworld/data/inpainting/predictions/train_test_run'
+wr_predictions_path = inpainting_root+'predictions/train_test_run'
 
 # Path to the pkl directory where datasets are stored
 pkl_path = save_path
 
 # Path to the directory where the model will be saved
-model_path = '/usr/local/data/sejacob/lifeworld/data/inpainting/models/'
+model_path = inpainting_root+'models/'
 
 execute.DCGAN_captions_LSTM_train_both(plt_path, wr_path, pkl_path, model_path, wr_predictions_path, 20, 1)
